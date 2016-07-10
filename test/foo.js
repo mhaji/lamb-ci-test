28f41b84e2d956017cf1cf581b24a76bbdf25587
Mukhtar Haji <mukhtar.haji@oracle.com>
2016-07-10 16:50:59 +0100

    Add unit test from mhart/test-ci-project

---
+++ test/foo.js
@@ -0,0 +1,19 @@
+var test = require('tape').test,
+        Foo = require('../lib/foo'),
+        foo;
+
+test('setup', function(t) {
+      foo = new Foo('baz');
+      t.ok(foo);
+      t.end();
+});
+
+test('bar', function(t) {
+      t.equal('baz', foo.bar());
+      t.end();
+});
+
+test('teardown', function(t) {
+      t.ok(true);
+      t.end();
+});
